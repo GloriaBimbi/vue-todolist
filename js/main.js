62f87913d3af2d0b074917becc49a49365c1c6c9
GloriaBimbi <gloria.bimbi1@libero.it>
2024-01-31 16:09:55 +0100

    feat descrizione

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 const { createApp } = Vue;
 const app = createApp({
   data() {
-    return {};
+    return {
+      toDoElements: [
+        {
+          text: "Fare la spesa",
+          done: false,
+        },
+        {
+          text: "Studiare per interrogazione",
+          done: false,
+        },
+        {
+          text: "Pulire casa",
+          done: false,
+        },
+        {
+          text: "Cucinare la cena",
+          done: false,
+        },
+        {
+          text: "Lavare i piatti sporchi",
+          done: false,
+        },
+        {
+          text: "Rispondere alle e-mail",
+          done: false,
+        },
+      ],
+    };
   },
 
   methods: {},
